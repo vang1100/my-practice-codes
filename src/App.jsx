6acd1b89234812320525abf37d2ba8dae7a9dc76
Padee Vang <vangpadee11@gmail.com>
2025-10-15 11:12:37 -0500

    created a file to practice coding

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Practice from './practice'
 
 function App() {
+
+  console.log('TESTING');
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+
+    <Practice/>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +32,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/Practice.jsx
@@ -0,0 +1,13 @@
+function Practice () {
+
+    console.log('this is the Practice.jsx file');
+
+    return (
+        <div>
+
+            PRACTICE
+        </div>
+    )
+}
+
+export default Practice;
