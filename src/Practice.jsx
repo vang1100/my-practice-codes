fb4daf89a2cc4d943c83fefe2cd2def9e4b221d2
Padee Vang <vangpadee11@gmail.com>
2025-10-17 10:20:31 -0500

    practing while loop

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -58,13 +58,116 @@ function Practice () {
         console.log("The" + " " + myStuff[stuff].item + " "+ "is the color" + " " + myStuff[stuff].color + ".");
     }
 
-    let numbers = [0, 3, 9, 10, 15, 19, 20, 37];
+    let numbers = [3, 9, 10, 15, 19, 20, 32, 37];
 
-    
+    for (let number of numbers) {
+
+        if ( number === 32) {
+
+            console.log('found my age');
+
+        } else  {
+
+            console.log('no age');
+        }
+
+
+    }
+
+    let oddNum = [];
+    let evenNum = [];
+
+    for (let i of numbers) {
+        if ( i % 2 !==0) {
+            oddNum.push(i);
+        } else if ( i % 2 == 0) {
+            evenNum.push(i);
+        }
+    }
+
+    console.log('these are the even numbers', evenNum);
+
 
 //    console.log(0+10);
 
 //    console.log(10+20);
+
+
+let randomNum = [1, 23, 1, 46, 9, 1];
+
+let index = 0;
+
+
+
+while ( index < randomNum.length) {
+
+    if (randomNum[index] === 1) {
+
+        randomNum.splice(index, 1);
+    
+
+    } else {
+
+         index++;
+
+    }
+
+   
+}
+
+ console.log('new array without 1s', randomNum);
+
+
+ let rats = [
+
+    {
+        object: 1,
+        infected: 'yes'
+    },
+
+    {
+        object: 2,
+        infected: 'yes'
+
+    },
+     
+    {
+        object: 3,
+        infected: 'no'
+
+    },
+
+    {
+        object: 4,
+        infected: 'n/a'
+    }
+ ];
+
+let infected = [];
+ let uninfected = [];
+ let noStatus = [];
+
+ let i = 0;
+
+ 
+//  while ( i < rats.length) {
+//     if (rats[i] === 'i');
+//  }
+
+
+while ( i < rats.length) {
+
+        if (rats[i].infected === 'yes') {
+
+              let infectedRats = rats.splice(i, 1);
+
+              console.log(infectedRats);
+        }
+
+    i++;
+}
+
+
 
     return (
         <div>
